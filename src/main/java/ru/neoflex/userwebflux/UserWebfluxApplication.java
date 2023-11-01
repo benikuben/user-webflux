9f2891572bfee129a84705f60570160d4cad43ef
Veronika Kulieva <benikuben@yandex.ru>
2023-11-01 18:02:37 +0300

    initial commit

---
+++ src/main/java/ru/neoflex/userwebflux/UserWebfluxApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.userwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserWebfluxApplication.class, args);
+    }
+
+}
